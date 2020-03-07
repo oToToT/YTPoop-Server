807583e1d25d03de5d12ae65cf4e85d2b1f0f68e
oToToT <ty1208chiang@gmail.com>
2020-03-07 21:57:09 +0800

    fix header.js

--- assets/javascripts/header.js
+++ assets/javascripts/header.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', function() {
-	document.getElementById('toggle-panel').addEventListener('click', function() {
-		let panel = document.getElementById('panel');
-		if (panel.style.display === 'none') {
-			panel.style.display = '';
-		} else {
-			panel.style.display = 'none';
-		}
-	});
+    let toggle = document.getElementById('toggle-panel');
+    if (toggle) {
+        toggle.addEventListener('click', function() {
+            let panel = document.getElementById('panel');
+            if (panel.style.display === 'none') {
+                panel.style.display = '';
+            } else {
+                panel.style.display = 'none';
+            }
+        });
+    }
 });
